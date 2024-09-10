7f206936e80ee78a46729a51042013afaba14a44
Nathan Teixeira <st10249266@vcconnect.edu.za>
2024-09-10 08:19:13 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prog_poe_st10249266.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
