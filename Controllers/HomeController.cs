3b3c9c48f4d747f0eb688c04f08556435c122fa5
Nathan Teixeira <130435264+Entropy-3@users.noreply.github.com>
2024-11-22 10:53:12 +0200

    added .style tags back for HTML

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -219,11 +219,11 @@ namespace prog_poe_st10249266.Controllers
             return RedirectToAction("ViewClaims");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
 
     }
 }
