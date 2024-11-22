3135bed660e518131cbcabbd88b2721b1d345453
Nathan Teixeira <130435264+Entropy-3@users.noreply.github.com>
2024-11-22 10:59:18 +0200

    edited autoprocessclaims method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -219,12 +219,6 @@ namespace prog_poe_st10249266.Controllers
             return RedirectToAction("ViewClaims");
         }
 
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
-
     }
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EOF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
